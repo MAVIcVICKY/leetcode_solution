759debd3d036f119a94dc5e065381186a1d252aa
VICKY KUMAR <eminevicky2013@gmail.com>
2025-12-25 14:59:10 +0530

    Time: 3 ms (56.65%), Space: 12.2 MB (9.97%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+bool dochechk(unordered_map<char,int>have,unordered_map<char,int>need){
+
+    for(auto it:need)
+    {
+        if(it.second>have[it.first])
+        return false;
+    }
+    return true;
+}
+
+
+
+
+    bool canConstruct(string ransomNote, string magazine) {
+
+        unordered_map<char,int>have;
+        unordered_map<char,int>need;
+
+        for(char ch:ransomNote)
+        {need[ch]++;  
+        }
+        
+        for(char ch:magazine)
+        {
+            have[ch]++;   
+        }
+
+       return dochechk(have,need);
+
+    }
+};
