a30f321d4838c7c15c588f0a3d2f28cb61f3f5dc
VICKY KUMAR <eminevicky2013@gmail.com>
2026-03-12 15:49:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -3,37 +3,25 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
 
         unordered_map<int,int> freq;
-        for (int x : nums) {
+        for(int x : nums)
             freq[x]++;
-        }
 
-        // min-heap by frequency
-        struct cmp {
-            bool operator()(const pair<int,int>& a,
-                            const pair<int,int>& b) {
-                return a.first > b.first; // smaller freq on top
-            }
-        };
+        priority_queue<pair<int,int>,
+                       vector<pair<int,int>>,
+                       greater<pair<int,int>>> pq;
 
-        priority_queue<
-            pair<int,int>,
-            vector<pair<int,int>>,
-            cmp
-        > pq;
+        for(auto &it : freq)
+        {
+            pq.push({it.second, it.first});
 
-        for (auto &it : freq) {
-            int num = it.first;
-            int frq = it.second;
-
-            pq.push({frq, num});
-
-            if (pq.size() > k) {
+            if(pq.size() > k)
                 pq.pop();
-            }
         }
 
         vector<int> res;
-        while (!pq.empty()) {
+
+        while(!pq.empty())
+        {
             res.push_back(pq.top().second);
             pq.pop();
         }
