525349ad03ef41f0dec13cb4a83c1f7c23646303
VICKY KUMAR <eminevicky2013@gmail.com>
2026-03-15 21:35:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,25 +2,43 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
 
-        vector<int>res;
-        int n=nums.size();
+        // vector<int>res;
+        // int n=nums.size();
 
-        unordered_map<int,int>mp;
+        // unordered_map<int,int>mp;
 
-        for(int i=0;i<n;i++)
-        {
-            int need=target-nums[i];
+        // for(int i=0;i<n;i++)
+        // {
+        //     int need=target-nums[i];
 
-            if(mp.find(need)!=mp.end())
-            {
-                res.push_back(mp[need]);
-                res.push_back(i);
-            }
-            mp[nums[i]]=i;
-        }
+        //     if(mp.find(need)!=mp.end())
+        //     {
+        //         res.push_back(mp[need]);
+        //         res.push_back(i);
+        //     }
+        //     mp[nums[i]]=i;
+        // }
 
 
-    
+
+
+vector<int>res;  //vector chaiye ek jha result store krega.
+
+unordered_map<int,int>mp;  //map bnayga ek jha joa needed hai with 
+
+int n=nums.size();
+
+for(int i=0;i<n;i++)
+{
+   int need=target-nums[i];
+
+    if(mp.find(need)!=mp.end())
+    {
+        res.push_back(i);
+        res.push_back(mp[need]);
+    }
+mp[nums[i]]=i;
+}
 
         
     return res;
